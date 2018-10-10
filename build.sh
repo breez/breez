e2395955600e37a8f4cac6b3d24f8fa80d3be03f
Yaacov Akiba Slama <ya@slamail.org>
2018-10-10 12:25:32 +0300

    Fix build script

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 # gomobile & gobind needs to be installed in $GOPATH/bin
 
 mkdir -p build/android
-PATH=$PATH:$GOPATH/bin ANDROID_HOME=~/devel/android/android-sdk-linux/ gomobile bind -target=android -tags="android" -o build/android/breez.aar github.com/breez/breez/bindings
+PATH=$PATH:$GOPATH/bin gomobile bind -target=android -tags="android" -o build/android/breez.aar github.com/breez/breez/bindings
