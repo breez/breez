e8399da4858df52b62ddeaf0902050c12f2f23a7
Igor Cota <igor@codexapertus.com>
2018-10-14 17:33:36 +0200

    Add funds via submarine swap

--- funds.go
+++ funds.go
@@ -3,22 +3,27 @@ package breez
 import (
 	"context"
 	"encoding/json"
+	"errors"
+	"github.com/btcsuite/btcd/chaincfg"
 	"io"
 	"time"
 
+	breezservice "github.com/breez/breez/breez"
 	"github.com/breez/breez/data"
 	"github.com/breez/lightninglib/lnrpc"
-	"github.com/golang/protobuf/proto"
-
-	breezservice "github.com/breez/breez/breez"
+	submarine "github.com/breez/submarinelib"
 )
 
 type swapAddressInfo struct {
 	Address              string
 	PaymentHash          []byte
+	PaymentPreimage      []byte
+	OurPubKey            []byte
+	OurPrivateKey        []byte
+	PayeePubKey          []byte
 	Transaction          string
-	TransactinoConfirmed bool
-	Payed                bool
+	TransactionConfirmed bool
+	Paid                 bool
 }
 
 func serializeSwapAddressInfo(s *swapAddressInfo) ([]byte, error) {
@@ -32,41 +37,89 @@ func deserializeSwapAddressInfo(addressBytes []byte) (*swapAddressInfo, error) {
 }
 
 /*
-AddFunds is responsible for topping up an existing channel
+AddFunds is responsible for topping up an existing channel via a submarine swap
 */
-func AddFunds(notificationToken string) (*data.AddFundReply, error) {
-	invoiceData := &data.InvoiceMemo{TransferRequest: true}
-	memo, err := proto.Marshal(invoiceData)
-	if err != nil {
-		return nil, err
-	}
+func AddFunds(notificationToken string) (string, error) {
+	// Generate two secrets
+	lightningPaymentHash, lightningPreimage := submarine.GenSecret()
+	chainPublicKey, chainPrivateKey, err := submarine.GenPublicPrivateKeypair()
 
-	invoice, err := lightningClient.AddInvoice(context.Background(), &lnrpc.Invoice{Memo: string(memo), Private: true, Expiry: 60 * 60 * 24 * 30})
-	if err != nil {
-		log.Criticalf("Failed to call AddInvoice %v", err)
-		return nil, err
-	}
-
+	// Send them to the server along with the notification token
 	c := breezservice.NewFundManagerClient(breezClientConnection)
 	ctx, cancel := context.WithTimeout(context.Background(), endpointTimeout*time.Second)
 	defer cancel()
 
-	r, err := c.AddFund(ctx, &breezservice.AddFundRequest{NotificationToken: notificationToken, PaymentRequest: invoice.PaymentRequest})
+	r, err := c.AddFund(ctx, &breezservice.AddFundRequest{
+		NotificationToken:  notificationToken,
+		LightningPublicKey: lightningPaymentHash[:],
+		ChainPublicKey:     chainPublicKey[:]})
 	if err != nil {
 		log.Errorf("Error in AddFund: %v", err)
-		return nil, err
+		return "", err
 	}
 
-	if r.Address != "" {
-		addressInfo := &swapAddressInfo{Address: r.Address, Payed: false, TransactinoConfirmed: false, PaymentHash: invoice.RHash}
-		err = saveSwapAddressInfo(addressInfo)
-		if err != nil {
-			return nil, err
-		}
+	// Create a script with server's data
+	script, err := submarine.GenSubmarineSwapScript(r.ChainPublicKey, chainPublicKey[:], lightningPaymentHash[:], 600)
+	if err != nil {
+		log.Errorf("Error in GenSubmarineSwapScript: %v", err)
+		return "", err
 	}
 
-	return &data.AddFundReply{Address: r.Address, MaxAllowedDeposit: r.MaxAllowedDeposit, ErrorMessage: r.ErrorMessage}, nil
+	// Determine which network we are running on
+	var network *chaincfg.Params
+
+	if cfg.Network == "testnet" {
+		network = &chaincfg.TestNet3Params
+	} else if cfg.Network == "simnet" {
+		network = &chaincfg.SimNetParams
+	} else if cfg.Network == "mainnet" {
+		network = &chaincfg.MainNetParams
+	} else {
+		return "", errors.New("unknown network type " + cfg.Network)
+	}
+
+	// Verify we are on the same page
+	if submarine.GenBase58Address(script, network)  != r.Address {
+		return "", errors.New("base58 address mismatch")
+	}
+
+	// Save everything to DB
+	addressInfo := &swapAddressInfo{
+		Address: r.Address,
+		Paid: false,
+		TransactionConfirmed: false,
+		PaymentHash: lightningPaymentHash,
+		PaymentPreimage: lightningPreimage,
+		OurPubKey: chainPublicKey,
+		OurPrivateKey: chainPrivateKey,
+		PayeePubKey: r.ChainPublicKey,
+	}
+
+	err = saveSwapAddressInfo(addressInfo)
+	if err != nil {
+		return "", err
+	}
+
+	return r.Address, nil
+
+	// Create an invoice when server tells us to... After someone paid into the address
+	// This is done on a different call
+
+	//invoiceData := &data.InvoiceMemo{TransferRequest: true}
+	//memo, err := proto.Marshal(invoiceData)
+	//if err != nil {
+	//	return "", err
+	//}
+
+	//invoice, err := lightningClient.AddInvoice(context.Background(), &lnrpc.Invoice{Memo: string(memo), Private: true, Expiry: 60 * 60 * 24 * 30})
+	//if err != nil {
+	//	log.Criticalf("Failed to call AddInvoice %v", err)
+	//	return "", err
+	//}
 }
+
+
+
 
 /*
 GetFundStatus gets a notification token and does two things:
@@ -99,7 +152,7 @@ func GetFundStatus(notificationToken string) (*data.FundStatusReply, error) {
 	var hasWaitingConfirmation bool
 	for address, status := range statusesMap.Statuses {
 		updateSwapAddressInfo(address, func(addressInfo *swapAddressInfo) {
-			addressInfo.TransactinoConfirmed = status.Confirmed
+			addressInfo.TransactionConfirmed = status.Confirmed
 			addressInfo.Transaction = status.Tx
 		})
 		if status.Confirmed {
@@ -178,7 +231,7 @@ func settlePendingTransfers() error {
 func getPaymentsForConfirmedTransactions() {
 	log.Infof("getPaymentsForConfirmedTransactions: asking for pending payments")
 	confirmedAddresses, err := fetchSwapAddresses(func(addr *swapAddressInfo) bool {
-		return addr.TransactinoConfirmed
+		return addr.TransactionConfirmed
 	})
 	if err != nil {
 		log.Errorf("getPaymentsForConfirmedTransactions: failed to fetch swap addresses %v", err)
--- go.mod
+++ go.mod
@@ -2,7 +2,9 @@ module github.com/breez/breez
 
 require (
 	github.com/awalterschulze/gographviz v0.0.0-20160912181450-761fd5fbb34e
+	github.com/boltdb/bolt v1.3.1 // indirect
 	github.com/breez/lightninglib v0.0.0-20180929220832-f1c03c1c7153
+	github.com/breez/submarinelib v0.0.0-20181007204205-01afefac5763
 	github.com/btcsuite/btcd v0.0.0-20180903232927-cff30e1d23fc
 	github.com/btcsuite/btclog v0.0.0-20170628155309-84c8d2346e9f
 	github.com/btcsuite/btcutil v0.0.0-20180706230648-ab6388e0c60a
@@ -11,7 +13,6 @@ require (
 	github.com/jessevdk/go-flags v1.4.0
 	github.com/urfave/cli v1.18.0
 	go.etcd.io/bbolt v1.3.0
-	golang.org/x/mobile v0.0.0-20180922163855-920b52be609a // indirect
 	golang.org/x/net v0.0.0-20180911220305-26e67e76b6c3
 	golang.org/x/sync v0.0.0-20180314180146-1d60e4601c6f
 	google.golang.org/grpc v1.14.0
