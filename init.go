85f60d164a362599d82ac2f064f39ba3d029cd30
Roei Erez <roeierez@gmail.com>
2018-11-13 14:02:47 +0200

    add client side pings to detect connection liveness

--- init.go
+++ init.go
@@ -20,6 +20,7 @@ import (
 	"github.com/breez/lightninglib/signal"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/keepalive"
 
 	"github.com/jessevdk/go-flags"
 )
@@ -95,7 +96,12 @@ func initBreezClientConnection() error {
 		return fmt.Errorf("credentials: failed to append certificates")
 	}
 	creds := credentials.NewClientTLSFromCert(cp, "")
-	con, err := grpc.Dial(cfg.BreezServer, grpc.WithTransportCredentials(creds))
+	con, err := grpc.Dial(cfg.BreezServer, grpc.WithTransportCredentials(creds), grpc.WithKeepaliveParams(
+		keepalive.ClientParameters{
+			PermitWithoutStream: true,
+			Time:                time.Second * 20,
+		},
+	))
 
 	//trace connection changes
 	go func() {
