551894a2ba40234970e7762e4f581751fc38f01a
Igor Cota <igor@codexapertus.com>
2018-11-19 09:46:43 +0100

    Add google drive backup / restore

---
+++ backup.go
@@ -0,0 +1,19 @@
+package breez
+
+import (
+	"github.com/breez/breez/data"
+	"github.com/breez/lightninglib/lnrpc"
+	"golang.org/x/net/context"
+	"path/filepath"
+)
+
+func updateBackupPaths() error {
+	response, err := lightningClient.GetBackup(context.Background(), &lnrpc.GetBackupRequest{})
+	if err != nil {
+		log.Errorf("Couldn't get backup: %v", err)
+		return err
+	}
+	log.Infof("Database backed up: %v", filepath.Dir(response.Files[0]))
+	notificationsChan <- data.NotificationEvent{Type: data.NotificationEvent_BACKUP_FILES_AVAILABLE, Data: filepath.Dir(response.Files[0])}
+	return nil
+}
