03dc9435f481793443717fea40e0a612f985b7f5
Roei Erez <roeierez@gmail.com>
2018-12-24 19:40:12 +0200

    implement channel open notification in account_notify

--- account_notify.go
+++ account_notify.go
@@ -9,14 +9,34 @@ import (
 )
 
 var (
-	openChanneNotificationTokens []string
-	subscriptionsSync            sync.Mutex
+	notification                         *notificationRequest
+	subscriptionsSync                    sync.Mutex
+	notificationTypeChannelOpened        = 1
+	notificationTypeReceivePaymentsReady = 2
 )
+
+type notificationRequest struct {
+	token            string
+	notificationType int
+}
 
 //RegisterReceivePaymentReadyNotification register in breez server for notification regarding the confirmation
 //of an existing pending channel. If there is not channels and no pending channels then this function waits for
-//for a channel to be opened by only adding the register logic to the paymentReadySubscriptions
+//for a channel to be opened.
 func RegisterReceivePaymentReadyNotification(token string) error {
+	return setUserNotificationRequest(token, notificationTypeReceivePaymentsReady)
+}
+
+//RegisterChannelOpenedNotification register in breez server for notification regarding the confirmation
+//of an existing pending channel. If there is not channels and no pending channels then this function waits for
+//for a channel to be opened.
+func RegisterChannelOpenedNotification(token string) error {
+	log.Infof("RegisterChannelOpenedNotification *****")
+	return setUserNotificationRequest(token, notificationTypeChannelOpened)
+}
+
+func setUserNotificationRequest(token string, notificationType int) error {
+	log.Infof("setUserNotificationRequest notificationType = %v", notificationType)
 	channelPoints, err := getBreezOpenChannelsPoints()
 	if err != nil {
 		return err
@@ -25,19 +45,63 @@ func RegisterReceivePaymentReadyNotification(token string) error {
 		return nil
 	}
 
-	registered, err := registerPendingChannelConfirmation(token)
-	if !registered {
-		//we have no channel and no pending channel with routing node, just save the token for later
-		subscriptionsSync.Lock()
-		defer subscriptionsSync.Unlock()
-		openChanneNotificationTokens = append(openChanneNotificationTokens, token)
+	subscriptionsSync.Lock()
+	defer subscriptionsSync.Unlock()
+	notification = &notificationRequest{
+		token:            token,
+		notificationType: notificationType}
+
+	return registerPendingChannelConfirmation()
+}
+
+func registerPendingChannelConfirmation() error {
+
+	pendingChannelPoint, err := getPendingBreezChannelPoint()
+	if err != nil {
+		return err
+	}
+	if pendingChannelPoint == "" {
+		return nil
 	}
 
-	return nil
+	c, ctx, cancel := getFundManager()
+	defer cancel()
+
+	subscriptionsSync.Lock()
+	currentRequest := notification
+	subscriptionsSync.Unlock()
+
+	log.Infof("registerPendingChannelConfirmation for token %v and notification type = %v", currentRequest.token, currentRequest.notificationType)
+
+	notificationTypeNeeded := breezservice.RegisterTransactionConfirmationRequest_READY_RECEIVE_PAYMENT
+	if currentRequest.notificationType == notificationTypeChannelOpened {
+		notificationTypeNeeded = breezservice.RegisterTransactionConfirmationRequest_CHANNEL_OPENED
+	}
+	_, err = c.RegisterTransactionConfirmation(ctx,
+		&breezservice.RegisterTransactionConfirmationRequest{
+			NotificationToken: currentRequest.token,
+			TxID:              strings.Split(pendingChannelPoint, ":")[0],
+			NotificationType:  notificationTypeNeeded,
+		})
+	if err != nil {
+		subscriptionsSync.Lock()
+		notification = nil
+		subscriptionsSync.Unlock()
+	}
+	return err
+}
+
+func onRoutingNodePendingChannel() {
+	registerPendingChannelConfirmation()
+	onAccountChanged()
+}
+
+func onRoutingNodeOpenedChannel() {
+	onAccountChanged()
 }
 
 func trackOpenedChannel() {
-	ticker := time.NewTicker(time.Minute * 1)
+	ticker := time.NewTicker(time.Second * 10)
 	for {
 		select {
 		case <-ticker.C:
@@ -51,39 +115,4 @@ func trackOpenedChannel() {
 			return
 		}
 	}
-}
-
-func registerPendingChannelConfirmation(token string) (bool, error) {
-
-	pendingChannelPoint, err := getPendingBreezChannelPoint()
-	if err != nil {
-		return false, err
-	}
-	if pendingChannelPoint == "" {
-		return false, nil
-	}
-
-	c, ctx, cancel := getFundManager()
-	defer cancel()
-	_, err = c.RegisterTransactionConfirmation(ctx,
-		&breezservice.RegisterTransactionConfirmationRequest{
-			NotificationToken: token,
-			TxID:              strings.Split(pendingChannelPoint, ":")[0],
-			NotificationType:  breezservice.RegisterTransactionConfirmationRequest_READY_RECEIVE_PAYMENT,
-		})
-	return err != nil, err
-}
-
-func onRoutingNodePendingChannel() {
-	subscriptionsSync.Lock()
-	defer subscriptionsSync.Unlock()
-	for _, token := range openChanneNotificationTokens {
-		registerPendingChannelConfirmation(token)
-	}
-	openChanneNotificationTokens = nil
-	onAccountChanged()
-}
-
-func onRoutingNodeOpenedChannel() {
-	onAccountChanged()
 }
