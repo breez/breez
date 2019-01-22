cd49686f3f02a4d38144c47234dca949b38049a6
Roei Erez <roeierez@gmail.com>
2019-01-22 17:00:23 +0200

    implement changes in sync job related to the shared chain service: 1. use shared service, don't stop. 2. use lightweight stop mechanism, don't wait 3. Remove WaitForShutdown function 4. Thread safety, Multiple jobs can run in parallel.

--- sync/job.go
+++ sync/job.go
@@ -1,83 +1,60 @@
 package sync
 
 import (
+	"fmt"
 	"sync/atomic"
 	"time"
 
+	"github.com/breez/breez/chainservice"
 	"github.com/btcsuite/btcd/wire"
 	"github.com/lightninglabs/neutrino"
 )
 
 /*
-Start starts the go routine for downloading the filters.
+Run executes the download filter operation synchronousely
 */
-func (s *Job) Start() error {
-	if atomic.AddInt32(&s.started, 1) != 1 {
-		return nil
-	}
-
-	db, chainService, err := newNeutrino(s.workingDir, s.network, &s.config)
-	if err != nil {
-		s.log.Errorf("Error creating ChainService: %s", err)
-		return err
-	}
-	neutrino.UseLogger(s.log)
-
-	s.db = db
-	s.neutrino = chainService
-
+func (s *Job) Run() error {
 	s.wg.Add(1)
-	go func() {
-		defer s.wg.Done()
-		err := s.syncFilters()
-		if err != nil {
-			s.log.Infof("sync finished with error %v", err)
-		}
-		s.terminate()
-	}()
+	defer s.wg.Done()
 
+	err := s.syncFilters()
+	if err != nil {
+		return fmt.Errorf("sync finished with error %v", err)
+	}
+	s.terminate()
 	return nil
 }
 
 /*
 Stop stops neutrino instance and wait for the syncFitlers to complete
 */
-func (s *Job) Stop() error {
-	if err := s.terminate(); err != nil {
-		s.log.Error("Error terminating job")
-		return err
-	}
-	s.WaitForShutdown()
-	return nil
-}
-
-/*
-WaitForShutdown blocks until the job completes gracefully
-*/
-func (s *Job) WaitForShutdown() {
+func (s *Job) Stop() {
+	s.terminate()
 	s.wg.Wait()
 }
 
-func (s *Job) terminate() error {
-	if atomic.AddInt32(&s.shutdown, 1) != 1 {
-		return nil
+func (s *Job) terminate() {
+	if atomic.AddInt32(&s.shutdown, 1) == 1 {
+		close(s.quit)
 	}
+}
 
-	if err := s.neutrino.Stop(); err != nil {
-		s.log.Error("Failed to stop neutrino from job")
+func (s *Job) terminated() bool {
+	select {
+	case <-s.quit:
+		return true
+	default:
+		return false
 	}
-	if err := s.db.Close(); err != nil {
-		s.log.Error("Failed close db from job")
-		return err
-	}
-
-	s.log.Infof("Job terminated succesfully")
-	return nil
 }
 
 func (s *Job) syncFilters() (err error) {
-	chainService := s.neutrino
-
+	s.log.Info("syncFilters started...")
+	chainService, err := chainservice.GetInstance(s.workingDir)
+	if err != nil {
+		s.log.Errorf("Error creating ChainService: %s", err)
+		return err
+	}
 	//get the best block before starting neutrino
 	//this will be the start point of the filters sync
 	startBlock, err := chainService.BestBlock()
@@ -85,14 +62,16 @@ func (s *Job) syncFilters() (err error) {
 		return err
 	}
 	chainService.Start()
-	defer func() {
-		err = chainService.Stop()
-	}()
 
 	//must wait for neutrino to connect to a peer and download the best
 	//block before starting the filters download loop.
 	for i := 0; i < 50 && chainService.IsCurrent(); i++ {
-		time.Sleep(time.Millisecond * 100)
+		select {
+		case <-time.After(time.Millisecond * 100):
+			continue
+		case <-s.quit:
+			return nil
+		}
 	}
 
 	//get the best block after letting neutrino some time
@@ -106,25 +85,43 @@ func (s *Job) syncFilters() (err error) {
 
 	//save last block in db
 	for currentHeight := startBlock.Height; currentHeight <= bestBlock.Height; currentHeight++ {
+		if s.terminated() {
+			return nil
+		}
+
+		// Get block hash
 		h, err := chainService.GetBlockHash(int64(currentHeight))
 		if err != nil {
 			s.log.Errorf("fail to fetch block hash", err)
 			return err
 		}
+		if s.terminated() {
+			return nil
+		}
+
+		// Get filter
 		_, err = chainService.GetCFilter(*h, wire.GCSFilterRegular, neutrino.PersistToDisk())
 		if err != nil {
 			s.log.Errorf("fail to download block filter", err)
 			return err
 		}
+		if s.terminated() {
+			return nil
+		}
 
 		//wait for the backend to sync if needed
 		for currentHeight == bestBlock.Height && !chainService.IsCurrent() {
-			time.Sleep(100 * time.Millisecond)
-			bestBlock, err = chainService.BestBlock()
-			if err != nil {
-				return err
+			select {
+			case <-time.After(time.Millisecond * 100):
+				bestBlock, err = chainService.BestBlock()
+				if err != nil {
+					return err
+				}
+			case <-s.quit:
+				return nil
 			}
 		}
 	}
+	s.log.Info("syncFilters completed succesfully")
 	return nil
 }
