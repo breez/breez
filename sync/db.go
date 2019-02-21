bcd02ec6feeb2fcb7125e8bb6cacac6df799c670
Roei Erez <roeierez@gmail.com>
2019-02-21 18:02:06 +0200

    sync: add rate limit functions in db.go
    In this commit we add two simple funtions get/set to the db
    struct for setting/getting the last successful run date.

--- sync/db.go
+++ sync/db.go
@@ -2,13 +2,15 @@ package sync
 
 import (
 	"encoding/binary"
+	"time"
 
 	bolt "go.etcd.io/bbolt"
 )
 
 const (
-	syncInfoBucket       = "syncInfo"
-	lastCFilterHeightKey = "lastCFilterHeight"
+	syncInfoBucket        = "syncInfo"
+	lastCFilterHeightKey  = "lastCFilterHeight"
+	lastSuccessRunDateKey = "lastSuccessRunDate"
 )
 
 type jobDB struct {
@@ -54,4 +56,33 @@ func (j *jobDB) fetchCFilterSyncHeight() (uint64, error) {
 		return nil
 	})
 	return startSyncHeight, err
+}
+
+func (j *jobDB) setLastSuccessRunDate(completeDate time.Time) error {
+	return j.db.Update(func(tx *bolt.Tx) error {
+		bucket, err := tx.CreateBucketIfNotExists([]byte(syncInfoBucket))
+		if err != nil {
+			return err
+		}
+		t := make([]byte, 8)
+		binary.BigEndian.PutUint64(t, uint64(completeDate.Unix()))
+		return bucket.Put([]byte(lastSuccessRunDateKey), t)
+	})
+}
+
+func (j *jobDB) lastSuccessRunDate() (time.Time, error) {
+	var lastRunDate uint64
+	err := j.db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(syncInfoBucket))
+		if bucket == nil {
+			return nil
+		}
+		dateBytes := bucket.Get([]byte(lastSuccessRunDateKey))
+		if dateBytes == nil {
+			return nil
+		}
+		lastRunDate = binary.BigEndian.Uint64(dateBytes)
+		return nil
+	})
+	return time.Unix(int64(lastRunDate), 0), err
 }
