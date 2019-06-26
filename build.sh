390d30cb09c6c38f6916995a4d526da8fb4563ac
Roei Erez <roeierez@gmail.com>
2019-06-26 11:38:17 +0300

    update build script

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 # gomobile & gobind needs to be installed in $GOPATH/bin
 
 mkdir -p build/android
-PATH=$PATH:$GOPATH/bin gomobile bind -target=android -tags="android experimental" -o build/android/breez.aar github.com/breez/breez/bindings
+PATH=$PATH:$GOPATH/bin gomobile bind -target=android -tags="android experimental signrpc walletrpc chainrpc invoicesrpc routerrpc backuprpc peerrpc submarineswaprpc breezbackuprpc" -o build/android/breez.aar github.com/breez/breez/bindings
