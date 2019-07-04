a56673d972d543f51b8fb60be6155f4a1cf03fa5
Roei Erez <roeierez@gmail.com>
2019-07-04 15:17:04 +0300

    temporary add query batch timeout to 300.

--- chainservice/init.go
+++ chainservice/init.go
@@ -61,8 +61,8 @@ func createService(workingDir string, breezDB *db.DB) (*neutrino.ChainService, r
 		logger = logBackend.Logger("CHAIN")
 		logger.Infof("After get logger")
 		logger.SetLevel(btclog.LevelDebug)
-		neutrino.UseLogger(logger)
-		neutrino.QueryTimeout = time.Second * 10
+		neutrino.UseLogger(logger)		
+		neutrino.QueryBatchTimeout = time.Second * 300
 	}
 	logger.Infof("creating shared chain service.")
 
