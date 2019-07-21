9f74046c42ae1f53076c0f56d6e1bfe9dfab0b53
Roei Erez <roeierez@gmail.com>
2019-07-21 14:19:04 +0300

    addfunds: confirmed addresses must have lockHeight less than current height.

--- swapfunds/addfunds.go
+++ swapfunds/addfunds.go
@@ -197,11 +197,12 @@ func (s *Service) GetFundStatus(notificationToken string) (*data.FundStatusReply
 		// In case the transactino is confirmed and has positive output, we will check
 		// if it is ready for completing the process by the client or it has error.
 		if a.ConfirmedAmount > 0 {
-			if a.LockHeight < info.BlockHeight && data.SwapError(a.SwapErrorReason) == data.SwapError_NO_ERROR {
+			if a.LockHeight > info.BlockHeight && data.SwapError(a.SwapErrorReason) == data.SwapError_NO_ERROR {
 				statusReply.ConfirmedAddresses = append(statusReply.ConfirmedAddresses, createRPCSwapAddressInfo(a))
 				continue
 			}
 
+			s.log.Infof("Adding refundable address: %v", string(jsonBytes))
 			statusReply.RefundableAddresses = append(statusReply.RefundableAddresses, createRPCSwapAddressInfo(a))
 		}
 	}
