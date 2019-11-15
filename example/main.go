99f7158cdd638f12b16ddfb788fe639754213254
Roei Erez <roeierez@gmail.com>
2019-11-15 23:53:03 +0200

    update example to use bindings package.

--- example/main.go
+++ example/main.go
@@ -5,50 +5,37 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/breez/breez"
-	"github.com/breez/breez/sync"
+	"github.com/breez/breez/bindings"
 )
 
-type Auth struct {
-	Token string
+type ServicesImpl struct {
 }
 
-func (a *Auth) SignIn() (string, error) {
-	return a.Token, nil
-}
+func (a *ServicesImpl) Notify(notificationEvent []byte) {}
 
-type AppServicesImpl struct {
-}
-
-func (a *AppServicesImpl) BackupProviderName() string {
-	return "gdrive"
-}
-
-func (a *AppServicesImpl) BackupProviderSignIn() (string, error) {
+func (a *ServicesImpl) BackupProviderSignIn() (string, error) {
 	return "", nil
+}
+
+func (a *ServicesImpl) BackupProviderName() string {
+	return "gdrive"
 }
 
 func main() {
 	workingDir := os.Getenv("LND_DIR")
-	app, err := breez.NewApp(workingDir, &AppServicesImpl{})
+	err := bindings.Init(os.TempDir(), workingDir, &ServicesImpl{})
 	if err != nil {
-		fmt.Println("Error creating App", err)
+		fmt.Println("Error in binding.Init", err)
 		os.Exit(1)
 	}
-	if err := app.Start(); err != nil {
-		fmt.Println("Error creating App", err)
+
+	err = bindings.Start()
+	if err != nil {
+		fmt.Println("Error in binding.Start", err)
 		os.Exit(1)
 	}
+
 	reader := bufio.NewReader(os.Stdin)
 	_, _ = reader.ReadString('\n')
 
-}
-
-func runJob(workingDir string) (r bool, err error) {
-	job, err := sync.NewJob(workingDir)
-	if err != nil {
-		return false, err
-	}
-
-	return job.Run()
 }
