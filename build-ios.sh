8f9952e4a9911a2166483d7820d1b011bd7c83b7
Roei Erez <roeierez@gmail.com>
2020-06-25 17:16:45 +0300

    add ldflags to strip symbols.

--- build-ios.sh
+++ build-ios.sh
@@ -3,4 +3,4 @@
 
 export GO111MODULE=off
 mkdir -p build/ios
-PATH=$PATH:$GOPATH/bin gomobile bind -target=ios -tags="ios experimental signrpc walletrpc chainrpc invoicesrpc routerrpc backuprpc peerrpc submarineswaprpc breezbackuprpc" -o build/ios/bindings.framework github.com/breez/breez/bindings
+PATH=$PATH:$GOPATH/bin gomobile bind -target=ios/arm64 -tags="ios experimental signrpc walletrpc chainrpc invoicesrpc routerrpc backuprpc peerrpc submarineswaprpc breezbackuprpc" -o build/ios/bindings.framework -ldflags="-s -w" github.com/breez/breez/bindings
