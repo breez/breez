c243768f42223988a1d33b2bc8b5d2d746ae7def
Roei Erez <roeierez@gmail.com>
2021-01-04 12:49:16 +0200

    sync unconfirmed channels using lsp info.

--- lsp_chan_state.go
+++ lsp_chan_state.go
@@ -5,37 +5,37 @@ import (
 	"encoding/hex"
 	"errors"
 	"fmt"
-	"strconv"
 	"strings"
 
+	"github.com/lightningnetwork/lnd/lnwire"
+
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
-	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/btclog"
 
 	breezservice "github.com/breez/breez/breez"
 	"github.com/breez/breez/channeldbservice"
 	"github.com/breez/breez/config"
+	"github.com/breez/breez/db"
 	"github.com/breez/breez/lnnode"
 	"github.com/breez/breez/services"
 	lspdrpc "github.com/breez/lspd/rpc"
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/golang/protobuf/proto"
 	"github.com/lightningnetwork/lnd/chainntnfs"
-	"github.com/lightningnetwork/lnd/channeldb"
 	"github.com/lightningnetwork/lnd/keychain"
 	"github.com/lightningnetwork/lnd/lnrpc/signrpc"
 )
 
 type peerSnapshot struct {
-	pubKey            string
-	unconfirmedOpen   map[string]uint64
-	unconfirmedClosed map[string]uint64
+	pubKey          string
+	unconfirmedOpen map[string]uint64
 }
 
 type lspChanStateSync struct {
 	cfg       *config.Config
 	log       btclog.Logger
 	breezAPI  services.API
+	breezDB   *db.DB
 	daemonAPI lnnode.API
 	snapshots map[string]*peerSnapshot
 }
@@ -45,6 +45,7 @@ func newLSPChanStateSync(app *App) *lspChanStateSync {
 		log:       app.log,
 		breezAPI:  app.ServicesClient,
 		daemonAPI: app.lnDaemon,
+		breezDB:   app.breezDB,
 		cfg:       app.cfg,
 		snapshots: make(map[string]*peerSnapshot, 0),
 	}
@@ -56,7 +57,19 @@ func (a *lspChanStateSync) recordChannelsStatus() error {
 		return err
 	}
 	a.snapshots = status
-	return nil
+	channels, err := a.breezDB.FetchMismatchedChannels()
+	if err != nil {
+		return err
+	}
+	if channels == nil || len(channels.ChanPoints) == 0 {
+		return nil
+	}
+	a.log.Infof("found channels mismatch to purge: %v", channels.ChanPoints)
+	if err := a.commitHeightHint(channels.LSPPubkey, channels.ChanPoints); err != nil {
+		a.log.Errorf("failed to purge height hint for channels: %v error: %v", channels.ChanPoints, err)
+	}
+	a.log.Infof("succesfully purged channels mismatch hints")
+	return a.breezDB.RemoveChannelMismatch()
 }
 
 func (a *lspChanStateSync) collectChannelsStatus() (map[string]*peerSnapshot, error) {
@@ -79,46 +92,31 @@ func (a *lspChanStateSync) collectChannelsStatus() (map[string]*peerSnapshot, er
 	}
 
 	for _, c := range channels {
-		if !c.ShortChanID().IsFake() && c.ChanStatus() == channeldb.ChanStatusDefault {
+		if !c.ShortChanID().IsFake() {
 			continue
 		}
 
-		a.log.Infof("asking status for channel id: %v", c.ShortChannelID.String())
-		if err != nil {
-			return nil, fmt.Errorf("failed to create height hint cache for channel %w", err)
-		}
-
+		a.log.Infof("collecting status for channel id: %v", c.ShortChannelID.String())
 		peerPubkey := hex.EncodeToString(c.IdentityPub.SerializeCompressed())
-		a.log.Infof("sync lsp channels for pubkey: %v", peerPubkey)
+		a.log.Infof("collecting lsp channels for pubkey: %v", peerPubkey)
 		snapshot, ok := snapshots[peerPubkey]
 		if !ok {
 			snapshot = &peerSnapshot{
-				pubKey:            peerPubkey,
-				unconfirmedOpen:   make(map[string]uint64, 0),
-				unconfirmedClosed: make(map[string]uint64, 0),
+				pubKey:          peerPubkey,
+				unconfirmedOpen: make(map[string]uint64, 0),
 			}
 			snapshots[peerPubkey] = snapshot
 		}
 
-		if c.ChanStatus() == channeldb.ChanStatusDefault {
-			height, err := hintCache.QueryConfirmHint(chainntnfs.ConfRequest{TxID: c.FundingOutpoint.Hash})
-			if errors.Is(err, chainntnfs.ErrConfirmHintNotFound) {
-				height = 0
-			} else if err != nil {
-				return nil, err
-			}
-			snapshot.unconfirmedOpen[c.FundingOutpoint.String()] = uint64(height)
-			a.log.Infof("adding unconfirmed channel to query %v hint=%v", c.FundingOutpoint.Hash.String(), height)
-		} else {
-			height, err := hintCache.QuerySpendHint(chainntnfs.SpendRequest{OutPoint: c.FundingOutpoint})
-			if errors.Is(err, chainntnfs.ErrConfirmHintNotFound) {
-				height = 0
-			} else if err != nil {
-				return nil, err
-			}
-			snapshot.unconfirmedClosed[c.FundingOutpoint.String()] = uint64(height)
-			a.log.Infof("adding waiting close channel to query %v hint=%v", c.FundingOutpoint.Hash.String(), height)
+		height, err := hintCache.QueryConfirmHint(chainntnfs.ConfRequest{TxID: c.FundingOutpoint.Hash})
+		if errors.Is(err, chainntnfs.ErrConfirmHintNotFound) {
+			height = 0
+		} else if err != nil {
+			return nil, err
 		}
+		snapshot.unconfirmedOpen[c.FundingOutpoint.String()] = uint64(height)
+		a.log.Infof("adding unconfirmed channel to query %v fundingHeight=%v hint=%v",
+			c.FundingOutpoint.Hash.String(), c.FundingBroadcastHeight, height)
 	}
 
 	return snapshots, nil
@@ -141,7 +139,6 @@ func (a *lspChanStateSync) syncChannels(lspNodePubkey string, lspPubkey []byte, 
 		return false, nil
 	}
 	mergedUnconfirmedOpen := make(map[string]uint64, 0)
-	mergedUnconfirmedClosed := make(map[string]uint64, 0)
 
 	for key, val := range afterStartSnapshot.unconfirmedOpen {
 		mergedUnconfirmedOpen[key] = val
@@ -150,31 +147,30 @@ func (a *lspChanStateSync) syncChannels(lspNodePubkey string, lspPubkey []byte, 
 			mergedUnconfirmedOpen[key] = hint
 		}
 	}
-	for key, val := range afterStartSnapshot.unconfirmedClosed {
-		mergedUnconfirmedClosed[key] = val
-		if hint, ok := beforeStartSnapshot.unconfirmedClosed[key]; ok {
-			a.log.Infof("setting height hint = %v for %v", hint, key)
-			mergedUnconfirmedClosed[key] = hint
-		}
-	}
-	confirmedOpen, confirmedClosed, err := a.checkChannels(mergedUnconfirmedOpen, mergedUnconfirmedClosed, lspPubkey, lspID)
+	confirmedOpen, _, err := a.checkChannels(mergedUnconfirmedOpen, map[string]uint64{}, lspPubkey, lspID)
 	if err != nil {
 		return false, err
 	}
 
-	mismatch := false
-	if len(confirmedOpen) > 0 {
-		mismatch = true
-		if err := a.purgeHeightHint(lspNodePubkey, confirmedOpen); err != nil {
-			a.log.Errorf("failed to purge height hint for opened channels: %v error: %v", confirmedOpen, err)
+	var mismatched []db.MismatchedChannel
+	for c, shortChanID := range confirmedOpen {
+		shortID := lnwire.NewShortChanIDFromInt(shortChanID)
+		a.log.Infof("got confirmed open for channel %v confirmationHeight = %v", c, shortID.BlockHeight)
+		if shortID.BlockHeight <= uint32(afterStartSnapshot.unconfirmedOpen[c]) {
+			a.log.Infof("found mismatched unconfirmed channel %v height hint = %v", c, afterStartSnapshot.unconfirmedOpen[c])
+			mismatched = append(mismatched, db.MismatchedChannel{
+				ChanPoint:   c,
+				ShortChanID: shortChanID,
+			})
 		}
 	}
-	if len(confirmedClosed) > 0 {
-		mismatch = true
-		if err := a.purgeHeightHint(lspNodePubkey, confirmedClosed); err != nil {
-			a.log.Errorf("failed to purge height hint for closed channels: %v error: %v", confirmedOpen, err)
-		}
+	if err := a.breezDB.SetMismatchedChannels(&db.MismatchedChannels{
+		LSPPubkey:  lspNodePubkey,
+		ChanPoints: mismatched,
+	}); err != nil {
+		return false, err
 	}
+	mismatch := len(mismatched) > 0
 	a.log.Infof("syncChannels finished mismatch = %v", mismatch)
 	return mismatch, nil
 }
@@ -241,12 +237,7 @@ func (a *lspChanStateSync) checkChannels(fakeChannels, waitingCloseChannels map[
 	return checkChannelsReply.NotFakeChannels, checkChannelsReply.ClosedChannels, nil
 }
 
-func (a *lspChanStateSync) purgeHeightHint(lspPubkey string, channelPoints map[string]uint64) error {
-	lspSnapshot, ok := a.snapshots[lspPubkey]
-	if !ok {
-		return nil
-	}
-
+func (a *lspChanStateSync) commitHeightHint(lspPubkey string, mismatched []db.MismatchedChannel) error {
 	chandb, cleanup, err := channeldbservice.Get(a.cfg.WorkingDir)
 	if err != nil {
 		return err
@@ -258,31 +249,21 @@ func (a *lspChanStateSync) purgeHeightHint(lspPubkey string, channelPoints map[s
 		QueryDisable: false,
 	}, chandb)
 
-	for tx := range channelPoints {
-		txParts := strings.Split(tx, ":")
+	for _, mismatch := range mismatched {
+		txParts := strings.Split(mismatch.ChanPoint, ":")
 		if len(txParts) != 2 {
 			return errors.New("invalid outpoint")
-		}
-
-		index, err := strconv.ParseUint(txParts[1], 10, 32)
-		if err != nil {
-			return errors.New("invalid tx index")
 		}
 		hash, err := chainhash.NewHashFromStr(txParts[0])
 		if err != nil {
 			return err
 		}
-		outpoint := wire.NewOutPoint(hash, uint32(index))
-		if _, ok := lspSnapshot.unconfirmedOpen[tx]; ok {
-			if err := hintCache.PurgeConfirmHint(chainntnfs.ConfRequest{TxID: *hash}); err != nil {
-				return err
-			}
-		}
-		if _, ok := lspSnapshot.unconfirmedClosed[outpoint.String()]; ok {
-			if err := hintCache.PurgeSpendHint(chainntnfs.SpendRequest{OutPoint: *outpoint}); err != nil {
-				return err
-			}
+		blockHeight := lnwire.NewShortChanIDFromInt(mismatch.ShortChanID).BlockHeight
+		if err := hintCache.CommitConfirmHint(blockHeight,
+			chainntnfs.ConfRequest{TxID: *hash}); err != nil {
+			return err
 		}
 	}
+
 	return nil
 }
