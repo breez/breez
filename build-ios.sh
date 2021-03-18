4d6df03a920043484cd5c85247acbf98011ac52f
Roei Erez <roeierez@gmail.com>
2021-03-18 16:03:42 +0200

    fix iOS build.

--- build-ios.sh
+++ build-ios.sh
@@ -1,6 +1,6 @@
 # GOPATH needs to be set
 # gomobile & gobind needs to be installed in $GOPATH/bin
 
-export GO111MODULE=off
+#export GO111MODULE=off
 mkdir -p build/ios
 PATH=$PATH:$GOPATH/bin gomobile bind -target=ios/arm64 -tags="ios experimental signrpc walletrpc chainrpc invoicesrpc routerrpc backuprpc peerrpc submarineswaprpc breezbackuprpc" -o build/ios/bindings.framework -ldflags="-s -w" github.com/breez/breez/bindings
