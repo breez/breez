def4da699cda0e4b5c4038562e5ec65a79a7f57c
Roei Erez <roeierez@gmail.com>
2021-04-21 13:50:30 +0300

    test changes

--- itest/tests/open_channel_test.go
+++ itest/tests/open_channel_test.go
@@ -3,17 +3,27 @@ package tests
 import (
 	"context"
 	"crypto/rand"
+	"encoding/hex"
+	"errors"
 	"fmt"
+	"log"
 	"strconv"
 	"strings"
 	"testing"
 	"time"
 
 	"github.com/breez/breez/data"
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/lightningnetwork/lnd/keychain"
 	"github.com/lightningnetwork/lnd/lnrpc"
 	"github.com/lightningnetwork/lnd/lnrpc/invoicesrpc"
+	"github.com/lightningnetwork/lnd/lnrpc/signrpc"
 	"github.com/lightningnetwork/lnd/lnrpc/walletrpc"
 	"github.com/lightningnetwork/lnd/lntypes"
+	"github.com/lightningnetwork/lnd/lnwire"
+	"github.com/lightningnetwork/lnd/zpay32"
 	"google.golang.org/grpc"
 )
 
@@ -928,4 +938,67 @@ func openBreezChannel(t *testing.T, test *framework,
 	if err != nil {
 		t.Fatalf("expected 1 channel")
 	}
+}
+
+func generateInvoiceWithNewAmount(payReq, nodeKey string, signerClient signrpc.SignerClient) (string, []byte, error) {
+	invoice, err := zpay32.Decode(payReq, &chaincfg.SimNetParams)
+	if err != nil {
+		return "", nil, fmt.Errorf("zpay32.Decode() error: %w", err)
+	}
+
+	pubkeyBytes, err := hex.DecodeString(nodeKey)
+	if err != nil {
+		return "", nil, err
+	}
+	pubKey, err := btcec.ParsePubKey(pubkeyBytes, btcec.S256())
+	if err != nil {
+		return "", nil, err
+	}
+
+	invoice.Features = lnwire.NewFeatureVector(lnwire.NewRawFeatureVector(), make(map[lnwire.FeatureBit]string, 0))
+	signer := zpay32.MessageSigner{SignCompact: func(hash []byte) ([]byte, error) {
+		kl := signrpc.KeyLocator{
+			KeyFamily: int32(keychain.KeyFamilyNodeKey),
+			KeyIndex:  0,
+		}
+
+		r, err := signerClient.SignMessage(context.Background(), &signrpc.SignMessageReq{Msg: hash, KeyLoc: &kl})
+		if err != nil {
+			return nil, fmt.Errorf("m.client.SignMessage() error: %w", err)
+		}
+		sig, err := btcec.ParseDERSignature(r.Signature, btcec.S256())
+		if err != nil {
+			return nil, fmt.Errorf("btcec.ParseDERSignature error: %w", err)
+		}
+		return toCompact(sig, pubKey, chainhash.HashB(hash))
+	}}
+	newInvoice, err := invoice.Encode(signer)
+	if err != nil {
+		log.Printf("invoice.Encode() error: %v", err)
+	}
+	return newInvoice, (*invoice.PaymentAddr)[:], err
+}
+
+func toCompact(sig *btcec.Signature, pubKey *btcec.PublicKey, hash []byte) ([]byte, error) {
+	curve := btcec.S256()
+	result := make([]byte, 1, curve.BitSize/4+1)
+	curvelen := (curve.BitSize + 7) / 8
+	bytelen := (sig.R.BitLen() + 7) / 8
+	if bytelen < curvelen {
+		result = append(result, make([]byte, curvelen-bytelen)...)
+	}
+	result = append(result, sig.R.Bytes()...)
+	bytelen = (sig.S.BitLen() + 7) / 8
+	if bytelen < curvelen {
+		result = append(result, make([]byte, curvelen-bytelen)...)
+	}
+	result = append(result, sig.S.Bytes()...)
+	for i := 0; i < (curve.H+1)*2; i++ {
+		result[0] = 27 + byte(i) + 4 // Add 4 because it's compressed
+		recoveredPubKey, _, err := btcec.RecoverCompact(curve, result, hash)
+		if err == nil && recoveredPubKey.IsEqual(pubKey) {
+			return result, nil
+		}
+	}
+	return nil, errors.New("The signature doesn't correspond to the pubKey")
 }
