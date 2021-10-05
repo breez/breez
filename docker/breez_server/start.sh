c890cfc96d0de2896abf40849dcc79f80791afd7
Daniel <dnlggr@users.noreply.github.com>
2021-10-05 12:24:33 +0200

    Fix dev env docker build

--- docker/breez_server/start.sh
+++ docker/breez_server/start.sh
@@ -2,6 +2,6 @@ sleep 20s
 ./generate_macaroon_hex LND /root/breez_node/data/chain/bitcoin/simnet/admin.macaroon /root/breez_node/tls.cert >> .env
 ./generate_macaroon_hex SUBSWAPPER_LND /root/subswap_node/data/chain/bitcoin/simnet/admin.macaroon /root/subswap_node/tls.cert >> .env
 cat .env
-/root/go/bin/migrate   -source file:///server/postgresql/migrations/ --database postgres://postgres:test@10.5.0.6:5432/postgres?sslmode=disable up 10
+/go/bin/migrate   -source file:///server/postgresql/migrations/ --database postgres://postgres:test@10.5.0.6:5432/postgres?sslmode=disable up 10
 
-exec /root/go/bin/godotenv ./server/server
+exec /go/bin/godotenv ./server/server
