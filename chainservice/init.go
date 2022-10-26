691eeb73df1870b4f63490d76ea155a800a88186
ruben beck <ruben.beck@pm.me>
2022-10-26 15:48:51 +0200

    functionality for adding, checking and testing restpeer

--- chainservice/init.go
+++ chainservice/init.go
@@ -3,6 +3,7 @@ package chainservice
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"path"
 	"strconv"
@@ -70,6 +71,26 @@ func TestPeer(peer string) error {
 		return err
 	}
 
+	// check if peer is a restpeer or regular peer before creating the neutrino
+	// and whenever it returs a valid filter.
+	if strings.HasPrefix(peer, "http") {
+		client := &http.Client{Timeout: 10 * time.Second}
+		hash := "0000000000000000000687bca986194dc2c1f949318629b44bb54ec0a94d8244"
+		res, err := client.Get(fmt.Sprintf("%v/rest/blockfilter/basic/%v.bin", peer, hash))
+		if err != nil {
+			logger.Errorf("Error getting CFilter from rest peer: %w", err)
+			return err
+		}
+		defer res.Body.Close()
+		if res.StatusCode == http.StatusOK {
+			logger.Infof("Successfully received CFilter adding %v to peers", peer)
+			return nil
+		} else {
+			logger.Error("error getting CFilter from rest peer")
+			return err
+		}
+	}
+
 	neutrinoConfig := neutrino.Config{
 		DataDir:      neutrinoDataDir,
 		Database:     db,
@@ -86,12 +107,14 @@ func TestPeer(peer string) error {
 		logger.Errorf("Error in chainService.Start: %v", err)
 		return err
 	}
+
 	time.Sleep(10 * time.Second)
 	c := chainService.ConnectedCount()
 	if c < 1 {
 		logger.Errorf("chainService.ConnectedCount() returned 0")
-		return fmt.Errorf("Cannot connect to peer")
+		return fmt.Errorf("cannot connect to peer")
 	}
+
 	return nil
 }
 
@@ -121,7 +144,19 @@ func createService(workingDir string, breezDB *db.DB) (*neutrino.ChainService, r
 		return nil, nil, err
 	}
 
-	service, walletDB, err = newNeutrino(workingDir, config, peers)
+	var restPeers []string
+	var p2pPeers []string
+	for _, p := range peers {
+		if strings.HasPrefix(p, "http") {
+			// since we found a rest peer we'll add it to the config
+			// and remove it from the peerslist in order to avoid duplication.
+			restPeers = append(restPeers, p)
+		} else {
+			p2pPeers = append(p2pPeers, p)
+		}
+	}
+
+	service, walletDB, err = newNeutrino(workingDir, config, p2pPeers, restPeers)
 	if err != nil {
 		logger.Errorf("failed to create chain service %v", err)
 		return nil, stopService, err
@@ -195,7 +230,7 @@ func parseAssertFilterHeader(headerStr string) (*headerfs.FilterHeader, error) {
 newNeutrino creates a chain service that the sync job uses
 in order to fetch chain data such as headers, filters, etc...
 */
-func newNeutrino(workingDir string, cfg *config.Config, peers []string) (*neutrino.ChainService, walletdb.DB, error) {
+func newNeutrino(workingDir string, cfg *config.Config, peers []string, restPeers []string) (*neutrino.ChainService, walletdb.DB, error) {
 	params, err := ChainParams(cfg.Network)
 
 	if err != nil {
@@ -206,6 +241,7 @@ func newNeutrino(workingDir string, cfg *config.Config, peers []string) (*neutri
 
 	neutrinoDataDir, db, err := GetNeutrinoDB(workingDir)
 	if err != nil {
+		logger.Infof("creating new neutrino service failed.")
 		return nil, nil, err
 	}
 	neutrinoConfig := neutrino.Config{
@@ -213,6 +249,7 @@ func newNeutrino(workingDir string, cfg *config.Config, peers []string) (*neutri
 		Database:     db,
 		ChainParams:  *params,
 		ConnectPeers: peers,
+		RestPeers:    restPeers,
 	}
 	logger.Infof("creating new neutrino service.")
 	chainService, err := neutrino.NewChainService(neutrinoConfig)
@@ -230,8 +267,12 @@ func GetNeutrinoDB(workingDir string) (string, walletdb.DB, error) {
 		return "", nil, err
 	}
 
+	logger.Infof("creating neutrino db at %v", workingDir)
 	db, err := walletdb.Create("bdb", neutrinoDB, false, time.Second*60)
-	fmt.Printf("neutrinoDB err = %v", err)
+	if err != nil {
+		logger.Infof("error creating neutrino db at %v, failed with %w", neutrinoDB, err)
+		return "", nil, err
+	}
 	return neutrinoDataDir, db, err
 }
 
