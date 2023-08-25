b50a93c70668f15605c5fc7958b9261ed894cd43
ruben beck <ruben.beck@pm.me>
2023-08-25 12:51:09 +0200

    remove unnecessary logging

--- backup/webdav_provider.go
+++ backup/webdav_provider.go
@@ -287,8 +287,6 @@ func (n *RemoteServerProvider) TestAuth() (err error) {
 	if err != nil {
 		return err
 	}
-
-	n.log.Infof("Got remoteserverprovider client: %v", client)
 	_, err = client.ListDir("/")
 	return err
 }
