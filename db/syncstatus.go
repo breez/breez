f3ca07bf5cbbf2e6a958c9253f55a883c10206f5
Jesse de Wit <witdejesse@hotmail.com>
2024-12-07 15:22:26 +0100

    remove record channels status
    The recordChannelsStatus function would insert all zero conf channels
    into the channelOpeningState bucket. This causes the funding workflow
    for these channels to restart. When the funding flow restarts, LND will
    start searching for the funding transaction by scanning the chain
    backwards. This means as you have channels for a longer time, Breez
    would consume more and more bandwidth to sync the chain from tip to
    channel open height. By deleting this function, once the funding
    workflow is complete once, it won't be entered again.
    
    Upon cleaning up it seems that SetMismatchedChannels was never used. So
    FetchMismatchedChannels and RemoveChannelMismatch could be removed as
    well.

--- db/syncstatus.go
+++ db/syncstatus.go
@@ -1,9 +1,5 @@
 package db
 
-import (
-	"encoding/json"
-)
-
 type MismatchedChannels struct {
 	LSPPubkey  string
 	ChanPoints []MismatchedChannel
@@ -26,31 +22,4 @@ func (db *DB) FetchLastSyncedHeaderTimestamp() (int64, error) {
 // SetLastSyncedHeaderTimestamp saves the last known header timestamp that the node is synced to.
 func (db *DB) SetLastSyncedHeaderTimestamp(ts int64) error {
 	return db.saveItem([]byte(syncstatus), []byte("last_header_timestamp"), itob(uint64(ts)))
-}
-
-func (db *DB) SetMismatchedChannels(mismatched *MismatchedChannels) error {
-	serialized, err := json.Marshal(mismatched)
-	if err != nil {
-		return err
-	}
-	return db.saveItem([]byte(syncstatus), []byte("mismatched_channels"), serialized)
-}
-
-func (db *DB) FetchMismatchedChannels() (*MismatchedChannels, error) {
-	raw, err := db.fetchItem([]byte(syncstatus), []byte("mismatched_channels"))
-	if err != nil {
-		return nil, err
-	}
-	if raw == nil {
-		return nil, nil
-	}
-	var mismatched MismatchedChannels
-	if err := json.Unmarshal(raw, &mismatched); err != nil {
-		return nil, err
-	}
-	return &mismatched, err
-}
-
-func (db *DB) RemoveChannelMismatch() error {
-	return db.deleteItem([]byte(syncstatus), []byte("mismatched_channels"))
 }
